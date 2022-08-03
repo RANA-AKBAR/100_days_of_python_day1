960ccd9b3a3146eda83797ac32c07c17ac07b3a3
RA <rana.akbar.08@gmail.com>
2022-08-03 21:52:30 +0100

    debugging

--- printing.py
+++ printing.py
@@ -3,7 +3,3 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
-
-
-
-
---
+++ testing.py
@@ -0,0 +1,8 @@
+# multiple lines string with new lines backglash
+print("Hello world!\nHello World!\n")
+
+#string concat 
+
+print("Rana" + " " + "Akbar")
+print("Rana " + "Akbar")
+print("Rana" + " Akbar")
