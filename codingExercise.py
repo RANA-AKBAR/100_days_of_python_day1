8a5afd9be6d420a008e701bc1765cd12fb4d03c8
RA <rana.akbar.08@gmail.com>
2022-08-10 14:19:45 +0100

    final exercise complete

---
+++ codingExercise.py
@@ -0,0 +1,6 @@
+name = input("Hello What is your name? ")
+
+print(len(name))
+
+
+print(len( input("What is your name? ") ) )
---
+++ variables.py
@@ -0,0 +1,9 @@
+from operator import le
+from re import L
+
+
+name = input("What is your name? ")
+
+length = len(name)
+
+print(length)
